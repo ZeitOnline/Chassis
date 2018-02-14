d5424386e9952f235a6b4a4ab96d530d5c6a09b6
Nico Bruenjes <nico.bruenjes@gmail.com>
2018-02-14 15:43:31 +0100

    Edit config examples

--- local-config-sample.php
+++ local-config-sample.php
@@ -3,7 +3,7 @@
 // local-config-db.php. Change these via your config.local.yaml instead.
 
 // Loopback connections can suck, disable if you don't need cron
-# define( 'DISABLE_WP_CRON', true );
+define( 'DISABLE_WP_CRON', true );
 
 // You'll probably want Automatic Updates disabled during development
 define( 'AUTOMATIC_UPDATER_DISABLED', true );
@@ -11,9 +11,5 @@ define( 'AUTOMATIC_UPDATER_DISABLED', true );
 // You'll probably want debug logging during development
 define( 'WP_DEBUG_LOG', true );
 
-// Multisite
-define( 'WP_ALLOW_MULTISITE', true );
-define('MULTISITE', true);
-
-define( 'WP_CONTENT_DIR', dirname( __FILE__ ) . '/wordpress-stuff' );
-define( 'WP_CONTENT_URL', 'http://' . $_SERVER['HTTP_HOST'] . '/wordpress-stuff' );
+define( 'WP_CONTENT_DIR', dirname( __FILE__ ) . '/content' );
+define( 'WP_CONTENT_URL', 'http://' . $_SERVER['HTTP_HOST'] . '/content' );
